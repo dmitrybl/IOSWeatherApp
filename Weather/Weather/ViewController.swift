2ad27ed5d902e6f571b6f659d5b90077a9e02a4b
Dmitry <dmitry.bl.5967@gmail.com>
2018-12-09 19:12:28 +0300

    data is loaded

---
+++ Weather/Weather/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Dmitry Belyaev on 09/12/2018.
+//  Copyright © 2018 Dmitry Belyaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var weatherImageIcon: UIImageView!
+    
+    let url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=8c031df7dbac4a3bd29b48b1aee1bb52"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let stringURL = URL(string: url)
+        let urlSession = URLSession.shared
+        let task = urlSession.dataTask(with: stringURL!) { (data, response, error) in
+            do {
+                let weatherJson = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers) as! NSDictionary
+                let weatherModel = WeatherModel(weatherJson: weatherJson)
+                
+                if error == nil {
+                    print(weatherModel.cityName)
+                    print(weatherModel.temperature)
+                    print(weatherModel.description)
+                    print(weatherModel.currentTime!)
+                    print(weatherModel.icon!)
+                    
+                    DispatchQueue.main.async {
+                        self.weatherImageIcon.image = weatherModel.icon!
+                    }
+                }
+            }
+            catch let jsonError {
+                print(jsonError)
+            }
+        }
+        task.resume()
+    }
+        
+}
+
---
+++ Weather/Weather/WeatherModel.swift
@@ -0,0 +1,73 @@
+//
+//  WeatherModel.swift
+//  Weather
+//
+//  Created by Dmitry Belyaev on 09/12/2018.
+//  Copyright © 2018 Dmitry Belyaev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherModel {
+    var cityName: String
+    var temperature: NSNumber
+    var description: String
+    var currentTime: String?
+    var icon: UIImage?
+    
+    init(weatherJson: NSDictionary) {
+        cityName = weatherJson["name"] as! String
+        let main = weatherJson["main"] as! NSDictionary
+        temperature = main["temp"] as! NSNumber
+        
+        let weather = weatherJson["weather"] as! NSArray
+        let d = weather[0] as! NSDictionary
+        description = d["description"] as! String
+        
+        let dt = weatherJson["dt"] as! Int
+        currentTime = getTime(timeMilliseconds: dt)
+        
+        let strIcon = d["icon"] as! String
+        icon = getIcon(stringIcon: strIcon)
+    }
+    
+    func getTime(timeMilliseconds: Int) -> String {
+        let interval = TimeInterval(timeMilliseconds)
+        let weatherDate = Date(timeIntervalSince1970: interval)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm"
+        
+        return dateFormatter.string(from: weatherDate)
+    }
+    
+    func getIcon(stringIcon: String) -> UIImage {
+        
+        let imageName: String
+        switch stringIcon {
+            case "01d": imageName = "01d"
+            case "02d": imageName = "02d"
+            case "03d": imageName = "03d"
+            case "04d": imageName = "04d"
+            case "09d": imageName = "09d"
+            case "10d": imageName = "10d"
+            case "11d": imageName = "11d"
+            case "13d": imageName = "13d"
+            case "50d": imageName = "50d"
+            case "01n": imageName = "01n"
+            case "02n": imageName = "02n"
+            case "03n": imageName = "03n"
+            case "04n": imageName = "04n"
+            case "09n": imageName = "09n"
+            case "10n": imageName = "10n"
+            case "11n": imageName = "11n"
+            case "13n": imageName = "13n"
+            case "50n": imageName = "50n"
+            default: imageName = "none"
+        }
+        
+        var iconImage = UIImage(named: imageName)
+        return iconImage!
+    }
+}
